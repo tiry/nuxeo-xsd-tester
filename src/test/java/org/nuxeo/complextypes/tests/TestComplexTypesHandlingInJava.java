87a28992360800ef01c87d533895aeac35464a90
Thierry Delprat <tdelprat@nuxeo.com>
2012-09-12 11:56:46 +0200

    Add restriction checks on 5.7

--- src/test/java/org/nuxeo/complextypes/tests/TestComplexTypesHandlingInJava.java
+++ src/test/java/org/nuxeo/complextypes/tests/TestComplexTypesHandlingInJava.java
@@ -24,10 +24,14 @@ import org.nuxeo.ecm.core.event.EventService;
 import org.nuxeo.ecm.core.schema.DocumentType;
 import org.nuxeo.ecm.core.schema.SchemaManager;
 import org.nuxeo.ecm.core.schema.types.ComplexType;
+import org.nuxeo.ecm.core.schema.types.Constraint;
 import org.nuxeo.ecm.core.schema.types.Field;
 import org.nuxeo.ecm.core.schema.types.ListType;
 import org.nuxeo.ecm.core.schema.types.Schema;
+import org.nuxeo.ecm.core.schema.types.SimpleType;
+import org.nuxeo.ecm.core.schema.types.SimpleTypeImpl;
 import org.nuxeo.ecm.core.schema.types.Type;
+import org.nuxeo.ecm.core.schema.types.constraints.EnumConstraint;
 import org.nuxeo.ecm.core.schema.types.primitives.StringType;
 import org.nuxeo.ecm.core.storage.sql.SQLRepositoryTestCase;
 import org.nuxeo.runtime.api.Framework;
@@ -106,9 +110,18 @@ public class TestComplexTypesHandlingInJava extends SQLRepositoryTestCase {
         assertNotNull(ct.getField("fieldType"));
         assertFalse(ct.getField("fieldType").getType().isComplexType());
         assertFalse(ct.getField("fieldType").getType().isListType());
-        // in 5.6 restrictions are not loaded in type def !
         assertEquals(StringType.ID,
                 ct.getField("fieldType").getType().getSuperType().getName());
+        // in 5.7 restrictions are now loaded in type def !
+        SimpleTypeImpl st = (SimpleTypeImpl) ct.getField("fieldType").getType();
+        Constraint[] constraints = st.getConstraints();
+        Constraint enumConstraint = constraints[0];
+        assertTrue(enumConstraint instanceof EnumConstraint);
+        EnumConstraint ec = (EnumConstraint) enumConstraint;
+        assertTrue(ec.getPossibleValues().contains("string"));
+        assertTrue(ec.getPossibleValues().contains("date"));
+        assertTrue(ec.getPossibleValues().contains("integer"));
+        assertTrue(ec.getPossibleValues().contains("real"));
 
         assertNotNull(ct.getField("roles"));
         assertFalse(ct.getField("roles").getType().isComplexType());
@@ -117,7 +130,6 @@ public class TestComplexTypesHandlingInJava extends SQLRepositoryTestCase {
         ListType ctlt = (ListType) ct.getField("roles").getType();
         Type subCT = ctlt.getFieldType();
         assertFalse(subCT.isComplexType());
-        // in 5.6 restrictions are not loaded in type def !
         assertEquals(StringType.ID, subCT.getSuperType().getName());
     }
 
